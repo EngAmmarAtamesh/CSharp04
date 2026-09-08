b616f3bb65aa5d4c38ffa8844e2f2cf25e431e97
EngAmmarAtamesh <am8952@fayoum.edu.eg>
2026-09-08 17:21:20 +0300

    Answer 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+
+            string title = "Refactoring";
+
+            switch (title)
+            {
+                case "Clean Code":
+                    Console.WriteLine("Great choice!");
+                    break;
+
+                case "Refactoring":
+                    Console.WriteLine("Nice pick!");
+                    break;
+
+                default:
+                    Console.WriteLine("Never heard of it");
+                    break;
+            }
+
+            #endregion
         }
     }
 }
